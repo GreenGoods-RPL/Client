239d8e3d5530f5bbf35cdbeb2a55e69a848d8579
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-10-22 14:14:44 +0700

    feat: add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+import logo from "@public/icons/GreenGoods_transparent.png"
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="font-montserrat">
+    <div className="font-montserrat p-10 lg:p-16">
+      <header className="w-full h-fit border-2 border-black">
+      <div className="flex items-center gap-2 relative h-16 w-16">
+        <Image src={logo} alt="Logo" fill sizes="50px" style={{ objectFit: "contain" }} />
+      </div>
+      </header>
+      <main className="">
         Hello World
       </main>
       <footer className="">
