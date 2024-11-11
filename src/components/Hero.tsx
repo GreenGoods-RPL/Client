05a8782b4845f5cc84b655238608a25e937d85de
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-11-11 14:45:07 +0700

    feat: add responsitivity to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,10 @@ import hero from "@public/images/Floating island 1.png";
 
 export default function Hero() {
   return (
-    <section className="py-12 px-28 bg-secondary rounded-3xl flex justify-between">
+    <section className="py-12 lg:px-28 sm:px-16 px-5 bg-secondary rounded-3xl flex sm:justify-between items-center sm:flex-row flex-col-reverse">
       <div className="flex flex-col justify-center">
         <p
-          className="mb-10 font-yanone font-bold text-[122px] text-light"
+          className="mb-10 font-yanone font-bold lg:text-[122px] text-5xl text-light"
           style={{ lineHeight: "1" }}
         >
           Live <br /> Healthy,
@@ -16,6 +16,7 @@ export default function Hero() {
         </p>
         <Button link="/about" text="Learn More" />
       </div>
+      <div className="lg:w-[500px] sm:w-[300px] w-[240px]">
       <Image
         src={hero}
         alt="Hero"
@@ -23,6 +24,7 @@ export default function Hero() {
         height={500}
         style={{ objectFit: "cover" }}
       />
+      </div>
     </section>
   );
 }
