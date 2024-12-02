686f9cc6a7a3ce3761833e6c8da0fb657dc387d0
Darren Adam Dewantoro <drnadam4611@gmail.com>
2024-12-02 14:11:29 +0700

    feat: completing transactionns page
    not perfect

--- src/app/transaction/page.tsx
+++ src/app/transaction/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import React, { useEffect, useState } from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function TransactionsPage() {
   const [transaction, setTransactions] = useState([]);
@@ -26,48 +28,36 @@ export default function TransactionsPage() {
 
   if (isLoading) {
     return (
-      <>
-        <Header />
         <div className="min-h-screen flex items-center justify-center">
           <p>Loading...</p>
         </div>
-        <Footer />
-      </>
     );
   }
 
   if (!transaction.length) {
     return (
-      <>
-        <Header />
         <div className="min-h-screen flex items-center justify-center">
           <p>Transactions not found.</p>
         </div>
-        <Footer />
-      </>
     );
   }
 
   return (
     <>
-      <Header />
-      <main>
-        <h1>Your Transactions</h1>
-
-        <div className="transaction-list">
+      <h1>Your Transactions</h1>
+  
+      <div className="transaction-list">
           {transaction.map((trans, index) => (
-            <div key={index} className="transaction-item">
+          <div key={index} className="transaction-item">
               <div className="product-info">
-            <h3>Product ID: {trans.product_id}</h3>
-            <p>Purchase Date: {trans.purchase_date}</p>
-            <p>Amount: {trans.amount}</p>
-            <p className="status">Status: {trans.status}</p>
+          <h3>Product ID: {trans.product_id}</h3>
+          <p>Purchase Date: {trans.purchase_date}</p>
+          <p>Amount: {trans.amount}</p>
+          <p className="status">Status: {trans.status}</p>
               </div>
-            </div>
+          </div>
           ))}
-        </div>
-      </main>
-      <Footer />
+      </div>
     </>
   );
 }
